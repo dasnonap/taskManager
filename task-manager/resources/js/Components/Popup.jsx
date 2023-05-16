e0d3edca2ee3fde14fe29c22b17b99e52fd92e51
dasnonap <ivanmihov971@gmail.com>
2023-05-16 22:12:10 +0300

    Fixed popup display

--- task-manager/resources/js/Components/Popup.jsx
+++ task-manager/resources/js/Components/Popup.jsx
@@ -8,7 +8,7 @@ export default function Popup({ openPopupButton, children, externalOpen }) {
     const [isOpened, setIsOpened] = useState(externalOpen);
 
     return (
-        <>
+        <div>
             <PrimaryButton
                 onClick={(e) => {
                     e.preventDefault();
@@ -21,34 +21,38 @@ export default function Popup({ openPopupButton, children, externalOpen }) {
 
             <div
                 className={
-                    `popup border border-1 rounded-md p-8 m-8 bg-white flex my-6 mx-auto flex-col items-center fixed h-fit inset-0 w-1/2 transition-all overflow-auto` +
+                    `fixed inset-0 bg-black/25` +
                     (isOpened === true ? ` block` : ` hidden`)
                 }
             >
-                <div className="mb-4 flex justify-content-center">
-                    <div></div>
+                <div
+                    className={`popup border border-1 rounded-md p-8 m-8 bg-white flex my-6 mx-auto flex-col items-center  w-1/2 transition-all overflow-auto relative`}
+                >
+                    <div className="mb-4 flex justify-content-center">
+                        <div></div>
 
-                    <h3 className="font-extrabold text-4xl">
-                        {openPopupButton}
-                    </h3>
+                        <h3 className="font-extrabold text-4xl">
+                            {openPopupButton}
+                        </h3>
 
-                    <PrimaryButton
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setIsOpened(!isOpened);
-                            console.log(isOpened);
-                        }}
-                        className="absolute top-8 right-5 group"
-                        type="button"
-                    >
-                        <FontAwesomeIcon
-                            icon="fa-solid fa-xmark"
-                            className="transition-all group-hover:rotate-45"
-                        />
-                    </PrimaryButton>
+                        <PrimaryButton
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setIsOpened(!isOpened);
+                                console.log(isOpened);
+                            }}
+                            className="absolute top-8 right-5 group"
+                            type="button"
+                        >
+                            <FontAwesomeIcon
+                                icon="fa-solid fa-xmark"
+                                className="transition-all group-hover:rotate-45"
+                            />
+                        </PrimaryButton>
+                    </div>
+                    {children}
                 </div>
-                {children}
             </div>
-        </>
+        </div>
     );
 }
